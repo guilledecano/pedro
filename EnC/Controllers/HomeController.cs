0b4753fd9e8d89737051a3b5cd9270e5bc97dda4
guilledecano <61856491+guilledecano@users.noreply.github.com>
2020-03-06 00:16:14 -0300

    Cambios iniciales

---
+++ EnC/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using EnC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EnC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            Persona unapersona = new Persona();
+            unapersona.edad = 25;
+            unapersona.nombre = "pedro";
+            return View(unapersona);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ EnC/Models/Persona.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EnC.Models
+{
+    public class Persona
+    {
+        public int edad { get; set; }
+        public string nombre { get; set; }
+    }
+}
